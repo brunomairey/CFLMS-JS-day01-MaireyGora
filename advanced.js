acac01767e14dde9dc9a68496c91a6dc593c4647
Christian Gora <christian.gora@student.tuwien.ac.at>
2020-05-25 11:22:14 +0200

    advanced finished

---
+++ advanced.js
@@ -0,0 +1,36 @@
+// Exercise 1
+
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+
+c=Number(c);
+d=Number(d);
+
+var sum_abcde=a+b+c+d+e;
+
+
+var f = '1';
+var g = 15;
+var h = 8;
+var i = "1";
+
+f=Number(f);
+i=Number(i);
+
+var product_fghi=f*g*h*i
+
+var divide_result=sum_abcde/product_fghi
+document.write(divide_result)
+
+
+// Exercise 2
+
+mulit_array=[ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ]
+console.log(mulit_array[1][1])
+console.log(mulit_array[4][2])
+console.log(mulit_array[5][3])
+console.log(mulit_array[2][3])
+console.log(mulit_array[2][1])
